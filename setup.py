748625351a2944fd44ec2db99ef025742d383453
Juho Kim <juho-kim@outlook.com>
2021-01-03 21:32:57 -0500

    Changed to version 0.0.7

--- setup.py
+++ setup.py
@@ -11,9 +11,12 @@ setuptools.setup(
     url='https://github.com/AussieSeaweed/django-sos',
     packages=setuptools.find_packages(),
     classifiers=[
-        'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     python_requires='>=3.9',
     install_requires=['Django'],
